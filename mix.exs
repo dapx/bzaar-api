60903f4f7dd6ed61a629639792a0b98b8dce04b9
Daniel André <dap1995@gmail.com>
2017-05-03 22:41:03 -0300

    Database and EndPoint address configuration to run server in heroku

--- mix.exs
+++ mix.exs
@@ -19,7 +19,7 @@ defmodule Bzaar.Mixfile do
   def application do
     [mod: {Bzaar, []},
      applications: [:phoenix, :phoenix_pubsub, :phoenix_html, :cowboy, :logger, :gettext,
-                    :phoenix_ecto, :mariaex]]
+                    :phoenix_ecto, :postgrex]]
   end
 
   # Specifies which paths to compile per environment.
@@ -33,7 +33,7 @@ defmodule Bzaar.Mixfile do
     [{:phoenix, "~> 1.2.0"},
      {:phoenix_pubsub, "~> 1.0"},
      {:phoenix_ecto, "~> 3.0"},
-     {:mariaex, ">= 0.0.0"},
+     {:postgrex, ">= 0.0.0"},
      {:phoenix_html, "~> 2.6"},
      {:phoenix_live_reload, "~> 1.0", only: :dev},
      {:gettext, "~> 0.11"},
