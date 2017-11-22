8b394687ed28ed8e6fb4bf39a7a3b6902e4f1a92
Daniel André da Silva <dap1995@gmail.com>
2017-11-22 14:14:05 -0200

    Add config

--- config/prod.secret.exs
+++ config/prod.secret.exs
@@ -15,3 +15,18 @@ config :bzaar, Bzaar.Repo,
   url: System.get_env("DATABASE_URL"),
   pool_size: String.to_integer(System.get_env("POOL_SIZE") || "10"),
   ssl: true
+
+config :facebook,
+  graph_url: "https://graph.facebook.com",
+  app_id: System.get_env("FACEBOOK_APP_ID"),
+  appsecret: System.get_env("FACEBOOK_APP_SECRET")
+
+config :ex_aws,
+  access_key_id: System.get_env("AWS_ACCESS_KEY_ID"),
+  secret_access_key: System.get_env("AWS_SECRET_ACCESS_KEY"),
+  bucket: System.get_env("AWS_BUCKET"),
+  region: "sa-east-1",
+  s3: [
+    scheme: "https://",
+    host: "s3.amazonaws.com",
+    region: "sa-east-1" ]
