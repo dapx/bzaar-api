7f90561bfeb1d87b4c445a028429817d5a691a02
Daniel André da Silva <dap1995@gmail.com>
2018-03-21 22:54:21 -0300

    Removed unnecessary ex_machina application

--- mix.exs
+++ mix.exs
@@ -19,7 +19,7 @@ defmodule Bzaar.Mixfile do
   def application do
     [mod: {Bzaar, []},
      applications: [:phoenix, :phoenix_pubsub, :phoenix_html, :cowboy, :logger, :gettext,
-                    :phoenix_ecto, :mariaex, :facebook, :ex_machina]]
+                    :phoenix_ecto, :mariaex, :facebook]]
   end
 
   # Specifies which paths to compile per environment.
