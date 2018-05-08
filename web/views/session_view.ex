4b0f1e92334335457da55af7fa10eea02c2e748f
Daniel André da Silva <dap1995@gmail.com>
2018-05-08 11:35:02 -0300

    Load user addresses on login

--- web/views/session_view.ex
+++ web/views/session_view.ex
@@ -1,12 +1,17 @@
 defmodule Bzaar.SessionView do
   use Bzaar.Web, :view
 
-  def render("login.json", params) do
-    %{user: params.user, jwt: params.jwt, exp: params.exp}
+  def render("show.json", %{ sessions: sessions }) do
+    %{data: render_many(sessions, Bzaar.SessionView, "login.json")}
   end
 
-  def render("error.json", user, jwt, exp) do
-    %{user: user, jwt: jwt, exp: exp}
+  def render("show.json", %{ session: session }) do
+    %{data: render_one(session, Bzaar.SessionView, "login.json")}
   end
 
+  def render("login.json", %{ session: session }) do
+    %{user: session.user,
+    jwt: session.jwt,
+    exp: session.exp}
+  end
 end
