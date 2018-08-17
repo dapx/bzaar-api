d45ce62e3a0a30f514a33ffdb1e25203e1f5b3f7
Daniel André da Silva <dap1995@gmail.com>
2018-08-17 19:09:15 -0300

    Rename Bzaar to BzaarWeb after upgrade phoenix to 1.3

--- lib/bzaar_web/views/error_helpers.ex
+++ lib/bzaar_web/views/error_helpers.ex
@@ -32,9 +32,9 @@ defmodule BzaarWeb.ErrorHelpers do
     #     dgettext "errors", "is invalid"
     #
     if count = opts[:count] do
-      Gettext.dngettext(Bzaar.Gettext, "errors", msg, msg, count, opts)
+      Gettext.dngettext(BzaarWeb.Gettext, "errors", msg, msg, count, opts)
     else
-      Gettext.dgettext(Bzaar.Gettext, "errors", msg, opts)
+      Gettext.dgettext(BzaarWeb.Gettext, "errors", msg, opts)
     end
   end
 end
